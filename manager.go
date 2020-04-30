0e8c1d660fe7840124ed57dfe42df2fc1bfa9850
manoelnecojr@gmail <necoengenheiro@gmail.com>
2020-04-30 15:01:21 -0300

    add logfile add alloc resourc

--- manager.go
+++ manager.go
@@ -3,39 +3,42 @@ package webhook
 import (
 	"git.resultys.com.br/lib/lower/exception"
 	"git.resultys.com.br/lib/lower/exec"
+	"git.resultys.com.br/lib/lower/logfile"
 	"git.resultys.com.br/lib/lower/net/request"
 	"git.resultys.com.br/motor/resource"
 )
 
 // Manager struct
 type Manager struct {
-	resource *resource.Resource
+	resource   *resource.Resource
+	logSuccess *logfile.Log
 }
 
-// New cria manager
+// New cria m
 func New(limit int) *Manager {
-	manager := &Manager{}
-	manager.resource = resource.New(limit)
-
-	return manager
+	return &Manager{
+		resource:   resource.New(limit),
+		logSuccess: logfile.New("webhook_success.log").Limit(1000),
+	}
 }
 
 // Trigger dispara um webhook
-func (manager *Manager) Trigger(url string, data interface{}) *Manager {
+func (m *Manager) Trigger(url string, data interface{}) *Manager {
 	if len(url) == 0 {
-		return manager
+		return m
 	}
 
-	// manager.resource.Alloc(func() {
-	exec.Tryx(3, func() {
-		msg, err := request.New(url).PostJSON(data)
-		if err != nil {
-			panic(msg)
-		}
-	}).Catch(func(err string) {
-		exception.Raise(err, exception.CRITICAL)
+	m.resource.Alloc(func() {
+		exec.Tryx(3, func() {
+			msg, err := request.New(url).PostJSON(data)
+			if err != nil {
+				panic(msg)
+			}
+			m.logSuccess.Add(url)
+		}).Catch(func(err string) {
+			exception.Raise(err, exception.CRITICAL)
+		})
 	})
-	// })
 
-	return manager
+	return m
 }
